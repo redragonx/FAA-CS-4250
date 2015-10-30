309c8ad949660f6d6962f7568fdbb38cb27e2d11
Josh <coyle5280@gmail.com>
2015-10-29 21:19:41 -0600

    framed out test files, added tests to data_verification, added plane object and folder added method stubs to collision and data verification classes - Josh

---
+++ program_objects/plane.py
@@ -0,0 +1,24 @@
+__author__ = 'group'
+
+
+class PlaneObject(object):
+
+    transponder_code = 0
+
+    name = ''
+
+    current_lat = 0
+
+    current_long = 0
+
+    current_altitude = 0
+
+    current_velocity = 0
+
+    def __init__(self, transponder_code, name, current_lat, current_long, current_altitude, current_velocity):
+        self.current_altitude = current_altitude
+        self.current_lat = current_lat
+        self.current_long = current_long
+        self.name = name
+        self.transponder_code = transponder_code
+        self.current_velocity = current_velocity
