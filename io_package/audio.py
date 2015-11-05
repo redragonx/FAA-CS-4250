70899efe15462f87270d5ecbda965ed39eea6758
daemoniclegend <auroraaxela@gmail.com>
2015-11-04 23:30:37 -0700

    Added test for audio_alert method in audio.py/test_audio.py

--- io_package/audio.py
+++ io_package/audio.py
@@ -2,22 +2,23 @@ __author__ = 'redragonx/daemoniclegend'
 
 # Sound files Copyright (C) 2004, Danny Ho, danniho@canada.com
 
-#import pyglet
+# import pyglet
 
 class Audio():
 
-    def audio_alert(error_type):
-        pass
+    def audio_alert(self, stringIn):
 
+        error_type = stringIn
 
-'''       if error_type = 'error':
-            sound = pyglet.resource.media('error1.wav')
-            sound.play()
-            pyglet.app.run()
-        elif error_type = 'bad_raw_data_error'
-            sound = pyglet.resource.media('error2.wav')
-            sound.play()
-            pyglet.app.run()
- '''
-
- #  eventually I will have this build out to play sounds depending on the error type.
+        if error_type == 'error1':
+            # sound = pyglet.resource.media('testok.wav')
+            # sound.play()
+            # pyglet.app.run()
+            return 'Valid'
+        elif error_type == 'bad_raw_data_error':
+            # sound = pyglet.resource.media('testfail.wav')
+            # sound.play()
+            # pyglet.app.run()
+            return 'Valid'
+        else:
+            return 'Invalid'
--- test/test_audio.py
+++ test/test_audio.py
@@ -1,13 +1,35 @@
 import unittest
+from io_package.audio import Audio
 
 
-__author__ = 'group'
+__author__ = 'redragonx/daemoniclegend'
 
 
 class TestAudio(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_audio_alert(self):
+        audio = Audio()
+
+        test1_answer = False
+        test2_answer = False
+        response = None
+        __goodString = 'error1'
+        __badString = 'Bad Error Message'
+
+    # tests using a valid error code
+        response = audio.audio_alert(__goodString)
+        if response == 'Valid':
+            test1_answer = True
+
+        print('Test with valid input: ', test1_answer)
+
+    # tests using an invalid error code
+        response = audio.audio_alert(__badString)
+        if response == 'Invalid':
+            test2_answer = True
+
+        print('Test with valid input: ', test2_answer)
+
 
 if __name__ == '__main__':
     unittest.main()
