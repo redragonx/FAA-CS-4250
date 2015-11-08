21a5a67a339ae575b44cd7b83e6a64af76305451
luludoobee <bcdean03@gmail.com>
2015-11-07 21:09:43 -0700

    Fixed Abel's Problems that we found

--- test/test_plane_controller.py
+++ test/test_plane_controller.py
@@ -2,6 +2,7 @@ __author__ = 'daemoniclegend'
 from mock import *
 from plane_controller.plane_ctrl import *
 from plane_controller import plane
+
 import unittest
 
 '''
@@ -14,13 +15,26 @@ mock_name.assert_called_once_with(name of paremeter)
 class TestPlaneController(unittest.TestCase):
 
     # patch.object if the method that you are testing is in a class
-    def test_plane_controller_driver(self,mock_input_data):
+    @patch.multiple("plane_controller.plane_ctrl",
+                    find_highest_priority_s = DEFAULT,
+                    get_corrective_action = DEFAULT,
+                    dispatch_collision_alerts = DEFAULT)
+    def test_plane_controller_driver(self, find_highest_priority_s,
+                                     get_corrective_action,
+                                     dispatch_collision_alert):
+        #1. find_highest_priority -> returns list of 1 or 2 planes
+        #2. corrective action -> String command
+        #3. dispatch event -> sends chris's audio alert
         # arrange
+        plane_location, plane_velocity = [0, 0, 0], [100, 100, 100]
+        plane_obj = self.plane_helper("0011", plane_location, plane_velocity)
+        data_in_collision_list = ["plane1", "plane2", "plane3"]
+
         # action
         # assert
         pass
 
-    @patch.multiple("plane_ctrl",
+    @patch.multiple("plane_controller.plane_ctrl",
                     convert_to_cartesian_meters = DEFAULT,
                     data_verify = DEFAULT,
                     update_plane_list = DEFAULT)
@@ -45,23 +59,32 @@ class TestPlaneController(unittest.TestCase):
             update_plane_list.assert_called_once_with(plane_obj)
         # pass
 
-    def test_convert_to_cartesian_meters(list_in):
-        pass
+    def test_convert_to_cartesian_meters(self):
+        original_data1 =[]
+        c_t_c1 =[]
+        self.assertEquals(convert_to_cartesian_meters(original_data1), c_t_c1)
+        original_data2 =[]
+        c_t_c2 =[]
+        self.assertEquals(convert_to_cartesian_meters(original_data2), c_t_c2)
+        original_data3 =[]
+        c_t_c3 =[]
+        self.assertEquals(convert_to_cartesian_meters(original_data3), c_t_c3)
 
-    def test_find_highest_priority_s(collsion_list):
-        pass
-
-    def test_update_plane_list(plane):
-        pass
-
-    def test_dispatch_collision_alerts(alert_type):
-        pass
-
-    def test_get_corrective_action(*planes):
-        pass
-
-    def test_update_transponder_code(new_code):
-        pass
+    #
+    # def test_find_highest_priority_s(collsion_list):
+    #     pass
+    #
+    # def test_update_plane_list(plane):
+    #     pass
+    #
+    # def test_dispatch_collision_alerts(alert_type):
+    #     pass
+    #
+    # def test_get_corrective_action(*planes):
+    #     pass
+    #
+    # def test_update_transponder_code(new_code):
+    #     pass
 
     def plane_helper(self, id_code, location_vector, velocity_vector):
         return plane.PlaneObject(id_code,
