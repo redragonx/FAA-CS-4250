402436678d93ef6263bffbdf0066037fe393543d
daemoniclegend <auroraaxela@gmail.com>
2015-11-08 06:30:47 -0700

    Housecleaning

--- test/test_user_keypad.py
+++ test/test_user_keypad.py
@@ -5,10 +5,7 @@ __author__ = 'group'
 
 
 class TestUserKeypad(unittest.TestCase):
-
-
     def test_keypad_input(self):
-
         keypad = User_keypad()
 
         test_string1 = '4538#'
@@ -19,17 +16,17 @@ class TestUserKeypad(unittest.TestCase):
         test_string6 = '#453#'
         test_string7 = '4536#45'
 
-
-    # test to see if a good input is processed correctly
+        # test to see if a good input is processed correctly
         self.assertEqual('Success', keypad.keypad_input(test_string1), "Test 1 Failed")
 
-    # tests 2-7 to see if a bad input is processed correctly
+        # tests 2-7 to see if a bad input is processed correctly
         self.assertEqual('Fail', keypad.keypad_input(test_string2), "Test 2 Failed")
         self.assertEqual('Fail', keypad.keypad_input(test_string3), "Test 3 Failed")
         self.assertEqual('Fail', keypad.keypad_input(test_string4), "Test 4 Failed")
         self.assertEqual('Fail', keypad.keypad_input(test_string5), "Test 5 Failed")
         self.assertEqual('Fail', keypad.keypad_input(test_string6), "Test 6 Failed")
         self.assertEqual('Fail', keypad.keypad_input(test_string7), "Test 7 Failed")
+
 
 if __name__ == '__main__':
     unittest.main()
