f08ca224602dacb9c3e816de07ebed6053d01393
abelamadou <abelamadou@msudenver.edu>
2015-11-07 23:10:23 -0700

    more coment

--- test/test_plane_controller.py
+++ test/test_plane_controller.py
@@ -18,65 +18,65 @@ mock_name.call_args_list =>
 
 class TestPlaneController(unittest.TestCase):
 
-    # patch.object if the method that you are testing is in a class
-    @patch.multiple("plane_controller.plane_ctrl",
-                    collision_detection_generator = DEFAULT,
-                    find_highest_priority_s = DEFAULT,
-                    get_corrective_action = DEFAULT,
-                    dispatch_collision_alerts = DEFAULT)
-    def test_plane_controller_driver(self, collision_detection_generator,
-                                     find_highest_priority_s,
-                                     get_corrective_action,
-                                     dispatch_collision_alerts):
-        #1. find_highest_priority -> returns list of 1 or 2 planes
-        #2. corrective action -> String command
-        #3. dispatch event -> sends chris's audio alert
-        # arrange
-
-        # action
-        plane_controller_driver()
-        # assert
-        self.assertTrue(collision_detection_generator.call_count > 0)
-        self.assertTrue(find_highest_priority_s.call_count > 0)
-        self.assertTrue(get_corrective_action.call_count > 0)
-        self.assertTrue(dispatch_collision_alerts > 0)
-        # pass
-
-    @patch.multiple("plane_controller.plane_ctrl",
-                    convert_to_cartesian_meters = DEFAULT,
-                    data_verify = DEFAULT,
-                    update_plane_list = DEFAULT)
-    def test_input_data(self,convert_to_cartesian_meters
-                        ,data_verify,update_plane_list):
-        #arrange
-            # data_in =[id,lat,long,altitude,x,y,z]
-            data_in =[10,111,222,2,0,1,0]
-            c_t_c =[]
-            plane_location, plane_velocity = [0, 0, 0], [100, 100, 100]
-            plane_obj = self.plane_helper("0011", plane_location, plane_velocity)
-
-        # action
-            input_data(data_in)
-
-        # assert
-            self.assertEqual(convert_to_cartesian_meters.call_count,1)
-            self.assertEqual(data_verify.call_count,1)
-            self.assertEqual(update_plane_list.call_count,1)
-            convert_to_cartesian_meters.assert_called_once_with(data_in)
-            data_verify.assert_called_once_with(c_t_c)
-            update_plane_list.assert_called_once_with(plane_obj)
-        # pass
-
-    def test_convert_to_cartesian_meters(self):
-        original_data1 =[]
-        c_t_c1 =[]
-        self.assertEquals(convert_to_cartesian_meters(original_data1), c_t_c1)
-        original_data2 =[]
-        c_t_c2 =[]
-        self.assertEquals(convert_to_cartesian_meters(original_data2), c_t_c2)
-        original_data3 =[]
-        c_t_c3 =[]
-        self.assertEquals(convert_to_cartesian_meters(original_data3), c_t_c3)
+    # # patch.object if the method that you are testing is in a class
+    # @patch.multiple("plane_controller.plane_ctrl",
+    #                 collision_detection_generator = DEFAULT,
+    #                 find_highest_priority_s = DEFAULT,
+    #                 get_corrective_action = DEFAULT,
+    #                 dispatch_collision_alerts = DEFAULT)
+    # def test_plane_controller_driver(self, collision_detection_generator,
+    #                                  find_highest_priority_s,
+    #                                  get_corrective_action,
+    #                                  dispatch_collision_alerts):
+    #     #1. find_highest_priority -> returns list of 1 or 2 planes
+    #     #2. corrective action -> String command
+    #     #3. dispatch event -> sends chris's audio alert
+    #     # arrange
+    #
+    #     # action
+    #     plane_controller_driver()
+    #     # assert
+    #     self.assertTrue(collision_detection_generator.call_count > 0)
+    #     self.assertTrue(find_highest_priority_s.call_count > 0)
+    #     self.assertTrue(get_corrective_action.call_count > 0)
+    #     self.assertTrue(dispatch_collision_alerts > 0)
+    #     # pass
+    #
+    # @patch.multiple("plane_controller.plane_ctrl",
+    #                 convert_to_cartesian_meters = DEFAULT,
+    #                 data_verify = DEFAULT,
+    #                 update_plane_list = DEFAULT)
+    # def test_input_data(self,convert_to_cartesian_meters
+    #                     ,data_verify,update_plane_list):
+    #     #arrange
+    #         # data_in =[id,lat,long,altitude,x,y,z]
+    #         data_in =[10,111,222,2,0,1,0]
+    #         c_t_c =[]
+    #         plane_location, plane_velocity = [0, 0, 0], [100, 100, 100]
+    #         plane_obj = self.plane_helper("0011", plane_location, plane_velocity)
+    #
+    #     # action
+    #         input_data(data_in)
+    #
+    #     # assert
+    #         self.assertEqual(convert_to_cartesian_meters.call_count,1)
+    #         self.assertEqual(data_verify.call_count,1)
+    #         self.assertEqual(update_plane_list.call_count,1)
+    #         convert_to_cartesian_meters.assert_called_once_with(data_in)
+    #         data_verify.assert_called_once_with(c_t_c)
+    #         update_plane_list.assert_called_once_with(plane_obj)
+    #     # pass
+    #
+    # def test_convert_to_cartesian_meters(self):
+    #     original_data1 =[]
+    #     c_t_c1 =[]
+    #     self.assertEquals(convert_to_cartesian_meters(original_data1), c_t_c1)
+    #     original_data2 =[]
+    #     c_t_c2 =[]
+    #     self.assertEquals(convert_to_cartesian_meters(original_data2), c_t_c2)
+    #     original_data3 =[]
+    #     c_t_c3 =[]
+    #     self.assertEquals(convert_to_cartesian_meters(original_data3), c_t_c3)
 
     @patch("plane_controller.plane_ctrl.")
     def test_find_highest_priority_s(self):
