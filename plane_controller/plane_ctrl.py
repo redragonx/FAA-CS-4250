ca348a12b9f6bd5c6d4eb1ec7b2eeb002a694471
abelamadou <abelamadou@msudenver.edu>
2015-11-08 00:53:36 -0700

    more coment

--- plane_controller/plane_ctrl.py
+++ plane_controller/plane_ctrl.py
@@ -75,6 +75,7 @@ def dispatch_collision_alerts(alert_type):
 
     :return:
     """
+    send_alert.audio_alert(alert_type)
     # rel_x = PA[loc_x] - new_plane[loc_x]
     # rel_y = PA[loc_y] - new_plane[loc_y]
     # rel_z = PA[loc_z] - new_plane[loc_z]
