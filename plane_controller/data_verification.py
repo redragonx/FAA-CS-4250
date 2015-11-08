cd8ba535c4978ed26e2baed391b91d63e3ae39c7
daemoniclegend <auroraaxela@gmail.com>
2015-11-08 05:22:41 -0700

    Added unit tests and implementation for initial data verification.

--- plane_controller/data_verification.py
+++ plane_controller/data_verification.py
@@ -1,26 +1,42 @@
 __author__ = 'redragonx/daemoniclegend'
 
-from plane_controller.plane import PlaneObject
+# from plane_controller.plane import PlaneObject
 import re
 
-class DataVerify():
 
+class DataVerify():
     def verify_data(self, list_in):
         data_list = list_in
-        idPattern = re.compile("\d{4}")
-        latPattern = re.compile("[0-1][0](([0-8]\d{8})|([9][0]{8}))")
-        longPattern = re.compile("[0-1][0-1](([0-7]\d{8})|([8][0]{7}))")
-        vectorPattern = re.compile("\d{8}")
-        VALID = True
-        if idPattern.match(data_list(0)):
-            if latPattern.match(data_list(1)):
-                if longPattern.match(data_list(2)):
-                    if vectorPattern.match(data_list(3)):
-                        if vectorPattern.match(data_list(4)):
-                            if vectorPattern.match(data_list(5)):
-                                verified_plane = PlaneObject(data_list(0), data_list(1), data_list(2), data_list(3),
-                                                             data_list(4), data_list(5))
-        return verified_plane, True
+        idPattern = re.compile("[0-9]{4}")
+        latPattern = re.compile("[01][0][0-8][0-9]{7}|[0-1][0][9][0]{7}")
+        longPattern = re.compile("[0-1][0-1][0-7][0-9]{6}|[0-1][0-1][8][0]{5}")
+        velocityPattern = re.compile("^[0-9]{6}")
+        vectorPattern = re.compile("^[0-9]{8}")
+        valid = False
+
+        if idPattern.match(data_list[0]):
+            if latPattern.match(data_list[1]):
+                if longPattern.match(data_list[2]):
+                    if velocityPattern.match(data_list[3]):
+                        if vectorPattern.match(data_list[4]):
+                            if vectorPattern.match(data_list[5]):
+                                if vectorPattern.match(data_list[6]):
+                                    # here is where the plane object itself would be returned after creation
+                                    return 'True'
+                                else:
+                                    return 'False'
+                            else:
+                                return 'False'
+                        else:
+                            return 'False'
+                    else:
+                        return 'False'
+                else:
+                    return 'False'
+            else:
+                return 'False'
+        else:
+            return 'False'
 
     def within_distance(self, plane1):
         return
--- test/test_data_verification.py
+++ test/test_data_verification.py
@@ -8,19 +8,19 @@ class test_data_verification(unittest.TestCase):
 
     def test_verify_data(self):
         dv = DataVerify()
-        __good_data_list = ["0001", "0174678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
-        __alpha_list = ["0A01", "0174678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
-        __bad_lat_list1 = ["0001", "0994678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
-        __bad_lat_list2 = ["0001", "2174678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
-        __bad_long_list1 = ["0001", "0174678922", "1905375468", "035700", "06007890", "46539201", "57890345"]
-        __bad_long_list2 = ["0001", "0174678922", "2045375468", "035700", "06007890", "46539201", "57890345"]
+        __good_data_list = ["0001", "0074678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
+        __alpha_list = ["0A01", "0074678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
+        __bad_lat_list1 = ["0001", "0094678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
+        __bad_lat_list2 = ["0001", "2074678922", "1045375468", "035700", "06007890", "46539201", "57890345"]
+        __bad_long_list1 = ["0001", "0074678922", "1085375468", "035700", "06007890", "46539201", "57890345"]
+        __bad_long_list2 = ["0001", "0074678922", "2045375468", "035700", "06007890", "46539201", "57890345"]
 
-        self.assertTrue(dv.verify_data(__good_data_list), 'Test 1 failed')
-        self.assertFalse(dv.verify_data(__alpha_list), 'Test 2 failed')
-        self.assertFalse(dv.verify_data(__bad_lat_list1), 'Test 3 failed')
-        self.assertFalse(dv.verify_data(__bad_lat_list2), 'Test 4 failed')
-        self.assertFalse(dv.verify_data(__bad_long_list1), 'Test 5 failed')
-        self.assertFalse(dv.verify_data(__bad_long_list2), 'Test 6 failed')
+        self.assertEqual('True', dv.verify_data(__good_data_list), 'Test 1 failed')
+        self.assertEqual('False', dv.verify_data(__alpha_list), 'Test 2 failed')
+        self.assertEqual('False', dv.verify_data(__bad_lat_list1), 'Test 3 failed')
+        self.assertEqual('False', dv.verify_data(__bad_lat_list2), 'Test 4 failed')
+        self.assertEqual('False', dv.verify_data(__bad_long_list1), 'Test 5 failed')
+        self.assertEqual('False', dv.verify_data(__bad_long_list2), 'Test 6 failed')
 
     def test_within_distance(self, plane1):
         return
