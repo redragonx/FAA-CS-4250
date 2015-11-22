4d9c58dbcf5684349f102bb445a57725609a1df8
luludoobee <bcdean03@gmail.com>
2015-11-22 09:42:19 -0700

    "Not yet implemented" removed

--- test/CovertBack.py
+++ test/CovertBack.py
@@ -88,7 +88,7 @@ print(build_number_system("0009", from_c_t_c1_back(x=-8000, y=0, z=6391000), -10
 # print len(build_number_system("0010", from_c_t_c1_back(x=500, y=500, z=(6391000-500)), -10, -10, 10))
 print(build_number_system("0010", from_c_t_c1_back(x=500, y=500, z=(6391000-500)), -10, -10, 10))
 
-
+#Tests
 # from_c_t_c1_back(x=, y=,z=)
 # from_c_t_c1_back(x=, y=,z=)
 # from_c_t_c1_back(x=, y=,z=)
